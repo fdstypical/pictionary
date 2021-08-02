1b3959fc07fff3ea346d42676272215f1dacbc66
Andrey Shulmin <ashulmin14@gmail.com>
2021-08-03 00:45:42 +0300

    fix optimization config

--- webpack/optimization.js
+++ webpack/optimization.js
@@ -1,23 +1,23 @@
 const optimization = {
   production: {
     splitChunks: {
-      chunks: "async",
-      minSize: 30000,
-      maxSize: 0,
+      chunks: 'async',
+      minSize: 20000,
+      maxSize: 30000,
       minChunks: 2,
       maxAsyncRequests: 5,
       maxInitialRequests: 3,
-      automaticNameDelimiter: "~",
+      automaticNameDelimiter: '~',
       name: false,
       cacheGroups: {
         vendors: {
-          filename: "[name].vendor.js",
+          filename: '[name].vendor.js',
           reuseExistingChunk: true,
           test: /[\\/]node_modules[\\/]/,
           priority: -10,
         },
         default: {
-          filename: "[name].main.js",
+          filename: '[name].main.js',
           minChunks: 2,
           priority: -20,
           reuseExistingChunk: true,

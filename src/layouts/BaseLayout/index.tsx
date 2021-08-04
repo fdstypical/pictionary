ecd6a75e1ada10e60fc7a1dc3c4c44303d55e1b4
Andrey Shulmin <ashulmin14@gmail.com>
2021-08-05 02:30:22 +0300

    add base-layout

---
+++ src/layouts/BaseLayout/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './style.styl';
+
+const BaseLayout: React.FC = () => (
+  <div className="base-layout l-container">
+    <main className="base-layout__main">
+      <h1>asd</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        nulla quo esse modi adipisci rem, dolorem amet officia illo vitae, ipsa
+        consequuntur iusto ab qui architecto eos totam quibusdam! Et.
+      </p>
+    </main>
+  </div>
+);
+
+export default BaseLayout;
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import '@/assets/stylus/index.styl';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const Hello: React.FC = () => <div>Hello</div>;
-
-const Main: React.FC = () => <div>Main</div>;
+import '../assets/stylus/index.styl';
+import BaseLayout from '../layouts/BaseLayout';
 
 const App: React.FC = () => (
   <Router>
     <Switch>
-      <Route path="/">
-        <Hello />
-      </Route>
-      <Route path="/main">
-        <Main />
-      </Route>
+      <Route exact path="/" component={BaseLayout} />
     </Switch>
   </Router>
 );

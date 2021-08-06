0bbd4264047bbf448788ba3e6c3ea742d9c5c5c7
Andrey Shulmin <ashulmin14@gmail.com>
2021-08-07 00:30:49 +0300

    fix alis and imports

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Header from '../../components/Header';
-import Logo from '../../components/Logo';
+import Header from '@/components/Header';
+import Logo from '@/components/Logo';
 
 import './style.styl';
 
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import '../assets/stylus/index.styl';
-import BaseLayout from '../layouts/BaseLayout';
+import '@/assets/stylus/index.styl';
+import BaseLayout from '@/layouts/BaseLayout';
 
 const App: React.FC = () => (
   <Router>

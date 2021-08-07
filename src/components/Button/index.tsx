5195133f7ea45012a49c4367d41c5a8ad11fd469
Andrey Shulmin <ashulmin14@gmail.com>
2021-08-07 16:26:45 +0300

    add base-button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import SvgIcon from '@/components/SvgIcon';
+import './style.styl';
+
+export enum ButtonThemes {
+  default = 'default',
+  plain = 'plain',
+  outline = 'outline',
+}
+
+export enum ButtonColors {
+  light = 'light',
+  dark = 'dark',
+  primary = 'primary',
+  secondary = 'secondary',
+}
+
+export enum ButtonSizes {
+  default = 'default',
+  small = 'small',
+  big = 'big',
+  large = 'large',
+}
+
+export enum ButtonShapes {
+  rounded = 'rounded',
+  square = 'square',
+  circle = 'circle',
+}
+
+export interface ButtonProps {
+  title?: string;
+  theme?: ButtonThemes;
+  color?: ButtonColors;
+  size?: ButtonSizes;
+  shape?: ButtonShapes;
+  icon?: string;
+  reverse?: boolean;
+  wide?: boolean;
+  dense?: boolean;
+  disabled?: boolean;
+  iconSettings?: { [key: string]: any };
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  title,
+  icon,
+  iconSettings,
+  theme = ButtonThemes.default,
+  color = ButtonColors.primary,
+  size = ButtonSizes.default,
+  shape = ButtonShapes.rounded,
+  reverse = false,
+  wide = false,
+  dense = false,
+  disabled = false,
+  children,
+  onClick,
+}) => {
+  const classNames = `base-button ${
+    theme ? `base-button--theme_${theme}` : ''
+  } ${color ? `base-button--color_${color}` : ''} ${
+    size ? `base-button--size_${size}` : ''
+  } ${shape ? `base-button--shape_${shape}` : ''} ${reverse ? 'reverse' : ''} ${
+    wide ? 'wide' : ''
+  } ${dense ? 'dense' : ''} ${icon && !title && !children ? 'only-icon' : ''}`;
+
+  return (
+    <button className={classNames} disabled={disabled} onClick={onClick}>
+      <div className="base-button__inner">
+        {icon && (
+          <div className="base-button__icon">
+            <SvgIcon name={icon} {...iconSettings} />
+          </div>
+        )}
+
+        {(children || title) && (
+          <span className="base-button__text">
+            {children}
+            {title}
+          </span>
+        )}
+      </div>
+    </button>
+  );
+};
+
+export default Button;
--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -3,14 +3,31 @@ import { Switch, Route } from 'react-router-dom';
 
 import Header from '@/components/Header';
 import Logo from '@/components/Logo';
+import Button, {
+  ButtonColors,
+  ButtonShapes,
+  ButtonSizes,
+  ButtonThemes,
+} from '@/components/Button';
 import Main from '@/views/Main';
-
 import './style.styl';
 
 const BaseLayout: React.FC = () => {
   return (
     <div className="base-layout">
-      <Header start={<Logo to="/" />} />
+      <Header
+        start={<Logo to="/" />}
+        end={
+          <Button
+            icon="menu"
+            theme={ButtonThemes.plain}
+            color={ButtonColors.secondary}
+            dense
+            iconSettings={{ stroked: true, iconSettings: { strokeWidth: 3 } }}
+            onClick={() => console.log('click')}
+          />
+        }
+      />
 
       <main className="base-layout__main">
         <div className="base-layout__content">

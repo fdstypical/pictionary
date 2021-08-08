f02c550e1d09555d4735b54d9981badc7dbfb059
Andrey Shulmin <ashulmin14@gmail.com>
2021-08-08 12:30:04 +0300

    fix layout

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -1,60 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Header from '@/components/Header';
 import Logo, { LogoColors, LogoThemes } from '@/components/Logo';
-import Button, { ButtonColors, ButtonThemes } from '@/components/base/Button';
 
 import Sidebar from '@/components/blocks/Sidebar';
 import Main from '@/views/Main';
 import './style.styl';
 
-const BaseLayout: React.FC = () => {
-  const [sidebar, toggleSidebar] = useState<boolean>(true);
-  const classNames = `base-layout base-layout--sidebar_${
-    sidebar ? 'open' : 'hidden'
-  }`;
+const BaseLayout: React.FC = () => (
+  <div className="base-layout">
+    <div className="base-layout__layout">
+      <Header
+        start={
+          <Logo to="/" color={LogoColors.secondary} theme={LogoThemes.italic} />
+        }
+      />
 
-  return (
-    <div className={classNames}>
-      <div className="base-layout__layout">
-        <Header
-          start={
-            <Logo
-              to="/"
-              color={LogoColors.secondary}
-              theme={LogoThemes.italic}
-            />
-          }
-          end={
-            <Button
-              dense
-              icon={sidebar ? 'close' : 'menu'}
-              theme={ButtonThemes.plain}
-              color={ButtonColors.secondary}
-              iconSettings={{
-                stroked: true,
-                iconSettings: { strokeWidth: 3 },
-              }}
-              onClick={() => toggleSidebar((prev) => !prev)}
-            />
-          }
-        />
-
-        <main className="base-layout__main">
-          <div className="base-layout__content">
-            <Switch>
-              <Route exact path="/" component={Main} />
-            </Switch>
-          </div>
-        </main>
-      </div>
-
-      <div className="base-layout__sidebar">
-        <Sidebar />
-      </div>
+      <main className="base-layout__main">
+        <div className="base-layout__content">
+          <Switch>
+            <Route exact path="/" component={Main} />
+          </Switch>
+        </div>
+      </main>
     </div>
-  );
-};
+
+    <div className="base-layout__sidebar">
+      <Sidebar />
+    </div>
+  </div>
+);
 
 export default BaseLayout;
